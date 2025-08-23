faf1452f04ce424940ee6520739d536377f44d8a
Chris Hall <explosivegnome@yahoo.com>
2025-08-23 20:34:18 +0800

    wip

--- tests/feature_tests.rs
+++ tests/feature_tests.rs
@@ -71,3 +71,22 @@ fn fate_dice() {
 	let mut actual_ave = sum as f64 / reps as f64;
 	assert!(actual_ave > -1. && actual_ave < 1., "average of {} rolls too far from the expected mean", reps);
 }
+
+#[test]
+fn exploding_dice_unlimited() {
+	let mut dice_bag = dicexp::new_dice_bag_from_seed(12345);
+	let exp = "1d6";
+	let expected = dicexp::DiceRoll{min: 1, max: 6 * 4, average: 3.5 + (1./6.)*(6.+3.5)};
+	let mut sum = 0;
+	let reps = 1000;
+	for _ in 0..reps {
+		let result = dice_bag.eval("(1d%-1)*10+(1d10-1)").unwrap();
+		assert_eq!(result.average, 499.5, "wrong average");
+		assert_eq!(result.min, 0, "wrong min");
+		assert_eq!(result.max, 999, "wrong max");
+		assert!(result.total >= result.min && result.total <= result.max, "total out of expected range");
+		sum += result.total;
+	}
+	let mut actual_ave = sum as f64 / reps as f64;
+	assert!(actual_ave > 500.*0.9 && actual_ave < 500.*1.1, "average of {} rolls too far from the expected mean", reps);
+}
